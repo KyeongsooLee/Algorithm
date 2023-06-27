9c2c32f22e0bbefe2343fe9372e1824122caaf0d
KyeongsooLee <leeksca26@gmail.com>
2023-06-27 00:34:46 -0400

    Add project files.

---
+++ TwoSum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoSum twoSum = new TwoSum();
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] result = twoSum.CalculateTwoSum(nums, target);
+
+            Console.WriteLine($"Index: {result[0]}, {result[1]}");
+        }
+    }
+}
