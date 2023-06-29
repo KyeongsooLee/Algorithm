a44cf90d85dac3cfafb3f3115e485c855c9fe6b4
KyeongsooLee <leeksca26@gmail.com>
2023-06-29 01:50:11 -0400

    RomanToInteger

---
+++ RomanToInteger/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RomanToInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RomanToInteger.Solution solution = new RomanToInteger.Solution();
+
+            String x = "MCMXCIV";
+            int RomanToInt = solution.RomanToInt(x);
+
+            Console.WriteLine($"{x} is {RomanToInt}");
+        }
+    }
+}
---
+++ RomanToInteger/RomanToInteger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanToInteger
+{
+    internal class RomanToInteger
+    {
+        public class Solution
+        {
+            public int RomanToInt(string s)
+            {
+                var map = new Dictionary<Char, int>()
+                {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+                s = s.Replace("IV", "IIII") // 4
+                .Replace("IX", "VIIII") // 9
+                .Replace("XL", "XXXX") // 40
+                .Replace("XC", "LXXXX") // 90
+                .Replace("CD", "CCCC") // 400
+                .Replace("CM", "DCCCC"); // 900
+
+                int sum = 0;
+                foreach (var c in s)
+                    sum = sum + map[c];
+                return sum;
+            }
+        }
+    }
+}
