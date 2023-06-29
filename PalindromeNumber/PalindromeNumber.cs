62fad6778211bd0ac997e0a6365081c74143f1c1
KyeongsooLee <leeksca26@gmail.com>
2023-06-28 21:42:48 -0400

    Two Sum, Palindrome Number

---
+++ PalindromeNumber/PalindromeNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber
+{
+    internal class PalindromeNumber
+    {
+        public class Solution
+        {
+            public bool IsPalindrome(int x)
+            {
+                string k = x.ToString();
+
+                for (int i = 0; i < k.Length / 2; i++)
+                {
+                    if (k[i] != k[k.Length - 1 - i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PalindromeNumber.Solution solution = new PalindromeNumber.Solution();
+
+            int x = 12321;
+            bool isPalindrome = solution.IsPalindrome(x);
+
+            Console.WriteLine($"{x} is a palindrome: {isPalindrome}");
+        }
+    }
+}
