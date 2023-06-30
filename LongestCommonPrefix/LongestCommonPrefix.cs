56504cf97c10e88adaff04993fbd88566ff19967
KyeongsooLee <leeksca26@gmail.com>
2023-06-30 16:01:13 -0400

    Longest Common Prefix

---
+++ LongestCommonPrefix/LongestCommonPrefix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestCommonPrefix
+{
+    internal class LongestCommonPrefix
+    {
+        public class Solution
+        {
+            public string LongestCommonPref(string[] strs)
+            {
+                int minLength = strs.Min(e => e.Length);
+                string result = strs.First(e => e.Length == minLength);
+
+                foreach (string str in strs)
+                {
+                    for (int i = 0; i < minLength; i++)
+                    {
+                        if (str[i] != result[i])
+                        {
+                            minLength = i;
+                            if (minLength == 0) return "";
+                        }
+                    }
+                }
+
+                return result.Substring(0, minLength);
+            }
+        }
+    }
+}
---
+++ LongestCommonPrefix/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LongestCommonPrefix
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        { 
+            LongestCommonPrefix.Solution solution = new LongestCommonPrefix.Solution();
+
+            string[] strs = new string[] { "flower", "flow", "flight" };
+            string result = solution.LongestCommonPref(strs);
+
+            Console.WriteLine($"The Longest common Prefix is {result}");
+        }
+    }
+}
