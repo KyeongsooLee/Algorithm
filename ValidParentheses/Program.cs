fc418212cc7b832e5a18998fe73e2f81f86d8051
KyeongsooLee <leeksca26@gmail.com>
2023-07-01 23:12:39 -0400

    Valid Parentheses

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidParentheses.Solution solution = new ValidParentheses.Solution();
+
+            string strs = "()[]{}";
+            bool result = solution.IsValid(strs);
+
+            Console.WriteLine($"Input: {strs}");
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
---
+++ ValidParentheses/ValidParentheses.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidParentheses
+{
+    internal class ValidParentheses
+    {
+        public class Solution
+        {
+            public bool IsValid(string s)
+            {
+
+                Dictionary<char, char> Parentheses = new Dictionary<char, char>() {
+                    { '(', ')' },
+                    { '[', ']' },
+                    { '{', '}' }
+                };
+
+                if (s.Length % 2 != 0 || s.Length == 0)
+                    return false;
+
+                Stack<char> stack = new Stack<char>();
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    char c = s[i];
+                    if (Parentheses.ContainsKey(c))
+                        stack.Push(c);
+                    else if ((stack.Count() == 0 || Parentheses[stack.Pop()] != c))
+                        return false;
+                }
+                return stack.Count() == 0;
+            }
+        }
+    }
+}
