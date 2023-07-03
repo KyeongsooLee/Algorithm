a4170a7859e707aec98838a5c6c2e03e1a51738d
KyeongsooLee <leeksca26@gmail.com>
2023-07-03 13:32:23 -0400

    Remove Duplicates From Sorted Array

---
+++ RemoveDuplicatesfromSortedArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RemoveDuplicatesfromSortedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 1, 2, 2, 3, 4, 4, 4, 5 };
+            Console.WriteLine("Original Array:");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write(nums[i] + " ");
+            }
+            RemoveDuplicatesfromSortedArray.Solution solution = new RemoveDuplicatesfromSortedArray.Solution();
+            int newLength = solution.RemoveDuplicates(nums);
+
+            Console.WriteLine("\nNew Array Length: " + newLength);
+            Console.WriteLine("Modified Array:");
+            for (int i = 0; i < newLength; i++)
+            {
+                Console.Write(nums[i] + " ");
+            }
+        }
+    }
+}
---
+++ RemoveDuplicatesfromSortedArray/RemoveDuplicatesfromSortedArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicatesfromSortedArray
+{
+    internal class RemoveDuplicatesfromSortedArray
+    {
+        public class Solution
+        {
+            public int RemoveDuplicates(int[] nums)
+            {
+                int k = 1;
+                int previous = nums[0];
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    if (nums[i] != previous)
+                    {
+                        nums[k] = nums[i];
+                        k++;
+                    }
+                    previous = nums[i];
+                }
+                return k;
+            }
+        }
+    }
+}
