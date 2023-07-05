a55924714d75f6d737fda714df2d38717cea11d3
KyeongsooLee <leeksca26@gmail.com>
2023-07-05 19:56:08 -0400

    Find the Index of the First Occurrence in a String

---
+++ Find the Index of the First Occurrence in a String/Find the Index of the First Occurrence in a String.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_the_Index_of_the_First_Occurrence_in_a_String
+{
+    internal class Find_the_Index_of_the_First_Occurrence_in_a_String
+    {
+        public class Solution
+        {
+            public int StrStr(string haystack, string needle)
+            {
+                if (haystack.Length < needle.Length)
+                    return -1;
+
+                int count = -1;
+                for (int i = 0; i < haystack.Length; i++)
+                {
+                    if (haystack[i] == needle[++count] && count == needle.Length - 1)
+                        return i - count;
+                    else if (haystack[i] != needle[count]) {
+                        i = i - count;
+                        count = -1;
+                    }
+                }
+                return -1;
+            }
+        }
+    }
+}
---
+++ Find the Index of the First Occurrence in a String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Find_the_Index_of_the_First_Occurrence_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Find_the_Index_of_the_First_Occurrence_in_a_String.Solution solution = new Find_the_Index_of_the_First_Occurrence_in_a_String.Solution();
+
+            string haystack = "mississippi";
+            string needle = "issip";
+            int result = solution.StrStr(haystack, needle);
+
+            Console.WriteLine($"haystack: {haystack}");
+            Console.WriteLine($"needle: {needle}");
+            Console.WriteLine($"Output: {result}");
+        }
+    }
+}
