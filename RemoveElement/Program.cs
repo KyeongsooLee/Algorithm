f093d67475fa86b7cfd319164f54d37ca5448361
KyeongsooLee <leeksca26@gmail.com>
2023-07-04 21:36:02 -0400

    Remove Element

---
+++ RemoveElement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static RemoveElement.RemoveElement;
+
+namespace RemoveElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2 }; // Input array
+            int val = 2; // Value to remove
+
+            Console.Write("Elements in array before removal: ");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
+
+            Solution solution = new Solution();
+            int k = solution.Remove_Element(nums, val);
+
+            Console.WriteLine($"\nTotal elements after Remove Element: {k}");
+
+            Console.Write("Elements in array after removal: ");
+            for (int i = 0; i < k; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
+        }
+    }
+}
---
+++ RemoveElement/RemoveElement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveElement
+{
+    internal class RemoveElement
+    {
+        public class Solution
+        {
+            public int Remove_Element(int[] nums, int val)
+            {
+                int k = 0;
+                foreach (int i in nums)
+                    if (i != val)
+                        nums[k++] = i;
+                return k;
+            }
+        }
+    }
+}
