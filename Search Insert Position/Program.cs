ace37b2121d1635355f01f0ec62a777c7d81b367
KyeongsooLee <leeksca26@gmail.com>
2023-07-06 15:45:52 -0400

    Search Insert Position

---
+++ Search Insert Position/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Search_Insert_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Search_Insert_Position.Solution solution = new Search_Insert_Position.Solution();
+
+            int[] nums = { 1, 3, 5, 6 }; // Input array
+            int target = 5;
+            Console.Write($"Input array: ");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if ( i == nums.Length - 1)
+                    Console.Write($"{nums[i]}");
+                else
+                    Console.Write($"{nums[i]}, ");
+            }
+            Console.WriteLine($"\nTarget: {target}");
+            int result = solution.SearchInsert(nums, target);
+
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
---
+++ Search Insert Position/Search Insert Position.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_Insert_Position
+{
+    internal class Search_Insert_Position
+    {
+        public class Solution
+        {
+            public int SearchInsert(int[] nums, int target)
+            {
+                int left = 0;
+                int right = nums.Length - 1;
+
+                while (left <= right)
+                {
+                    int mid = (left + right) / 2;
+                    if (nums[mid] == target)
+                    {
+                        return mid;
+                    }
+                    else if (nums[mid] < target)
+                    {
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+                return left;
+            }
+        }
+    }
+}
