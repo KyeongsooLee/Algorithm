e440b0a51db6411218fbf287cdf8fbfbf8c749f7
KyeongsooLee <leeksca26@gmail.com>
2023-07-07 19:57:18 -0400

    Length of Last Word

---
+++ Length of Last Word/Length of Last Word.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Length_of_Last_Word
+{
+    internal class Length_of_Last_Word
+    {
+        public class Solution
+        {
+            public int LengthOfLastWord(string s)
+            {
+                int count = 0;
+                for (int i = s.Length - 1; i >= 0; i--)
+                {
+                    if (s[i] == ' ' && count != 0)
+                        return count;
+                    if (s[i] != ' ')
+                        count++;
+                }
+                return count;
+            }
+        }
+    }
+}
---
+++ Length of Last Word/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Length_of_Last_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Length_of_Last_Word.Solution solution = new Length_of_Last_Word.Solution();
+
+            string s = "   fly me   to   the moon  ";
+            int result = solution.LengthOfLastWord(s);
+            Console.WriteLine($"Input string:[{s}]");
+            Console.WriteLine($"The length of the last word: {result}");
+        }
+    }
+}
