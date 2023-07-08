475e17355e8e469e828493db1cc83961db3eef89
KyeongsooLee <leeksca26@gmail.com>
2023-07-08 16:14:44 -0400

    Plus One

---
+++ Plus One/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Plus_One
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Plus_One.Solution solution = new Plus_One.Solution();
+
+            int[] digits = { 9, 9, 9 };
+            Console.Write("Input digits: [ ");
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (i != digits.Length - 1)
+                    Console.Write($"{digits[i]}, ");
+                else
+                    Console.Write($"{digits[i]} ]\n");
+            }
+            int[] output = solution.PlusOne(digits);
+            Console.Write("Output digits: [ ");
+            for (int i = 0; i < output.Length; i++)
+            {
+                if (i != output.Length - 1)
+                    Console.Write($"{output[i]}, ");
+                else
+                    Console.Write($"{output[i]} ]\n");
+            }
+        }
+    }
+}
