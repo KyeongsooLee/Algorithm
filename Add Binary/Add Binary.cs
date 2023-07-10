4be1f6286e2ac3efaa7772c587530b63b206244c
KyeongsooLee <leeksca26@gmail.com>
2023-07-09 20:33:21 -0400

    Add Binary

---
+++ Add Binary/Add Binary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_Binary
+{
+    internal class Add_Binary
+    {
+        public class Solution
+        {
+            public string AddBinary(string a, string b)
+            {
+                int length = Math.Max(a.Length, b.Length);
+
+                a = a.PadLeft(length, '0');
+                b = b.PadLeft(length, '0');
+
+                string result = "";
+                bool carry = false;
+
+                for (int i = length - 1; i >= 0; i--)
+                {
+                    if (a[i] == '1' && b[i] == '1')
+                    {
+                        result = (carry ? "1" : "0") + result;
+                        carry = true;
+                    }
+                    else if (a[i] == '1' || b[i] == '1')
+                    {
+                        result = (carry ? "0" : "1") + result;
+                        carry = carry && (a[i] == '1' || b[i] == '1');
+                    }
+                    else
+                    {
+                        result = (carry ? "1" : "0") + result;
+                        carry = false;
+                    }
+                }
+
+                if (carry) result = "1" + result;
+
+                return result;
+            }
+        }
+    }
+}
---
+++ Add Binary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Add_Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add_Binary.Solution solution = new Add_Binary.Solution();
+
+            string a = "1010";
+            string b = "1011";
+            string output = solution.AddBinary(a, b);
+
+            Console.WriteLine($"String a:           [{a}]");
+            Console.WriteLine($"String b:           [{b}]");
+            Console.WriteLine($"Add Binary Result: [{output}]");
+        }
+    }
+}
