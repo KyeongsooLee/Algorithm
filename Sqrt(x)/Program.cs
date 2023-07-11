7f49f0444ef0182371e55ab0374ac587afaec86b
KyeongsooLee <leeksca26@gmail.com>
2023-07-10 22:01:50 -0400

    Sqrt(x)

---
+++ Sqrt(x)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sqrt_x_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sqrt_x_.Solution solution = new Sqrt_x_.Solution();
+
+            int x = 8;
+            int output = solution.MySqrt(x);
+
+            Console.WriteLine($"Input : {x}");
+            Console.WriteLine($"Output: {output}");
+        }
+    }
+}
---
+++ Sqrt(x)/Sqrt(x).cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sqrt_x_
+{
+    internal class Sqrt_x_
+    {
+        public class Solution
+        {
+            public int MySqrt(int x)
+            {
+                if (x == 0)
+                    return 0;
+
+                int left = 1;
+                int right = x;
+
+                while (left <= right)
+                {
+                    int mid = left + (right - left) / 2;
+                    if (mid == x / mid)
+                        return mid;
+                    else if (mid < x / mid)
+                        left = mid + 1;
+                    else
+                        right = mid - 1;
+                }
+
+                return right;
+            }
+        }
+    }
+}
