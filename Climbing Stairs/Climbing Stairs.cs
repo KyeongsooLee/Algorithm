ed3e2ad0aeb952f7026a1b52fd333d6a78650897
KyeongsooLee <leeksca26@gmail.com>
2023-07-12 11:22:11 -0400

    Climbing Stairs

---
+++ Climbing Stairs/Climbing Stairs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Climbing_Stairs
+{
+    internal class Climbing_Stairs
+    {
+        public class Solution
+        {
+            public int ClimbStairs(int n)
+            {
+                if (n <= 1)
+                    return 1;
+
+                int[] dp = new int[n + 1];
+                dp[0] = 1;
+                dp[1] = 1;
+
+                for (int i = 2; i <= n; i++)
+                {
+                    dp[i] = dp[i - 1] + dp[i - 2];
+                }
+
+                return dp[n];
+            }
+        }
+    }
+}
---
+++ Climbing Stairs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Climbing_Stairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Climbing_Stairs.Solution solution = new Climbing_Stairs.Solution();
+
+            int n = 5;
+            int output = solution.ClimbStairs(n);
+
+            Console.WriteLine($"Input n: {n}");
+            Console.WriteLine($"Output : {output}");
+        }
+    }
+}
