7539210a60e9d7150eff6bfef3150319e947568a
KyeongsooLee <leeksca26@gmail.com>
2023-07-15 19:59:12 -0400

    Binary Tree Inorder Traversal

---
+++ Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Tree_Inorder_Traversal
+{
+    internal class Binary_Tree_Inorder_Traversal
+    {
+        public class TreeNode {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+ 
+        public class Solution
+        {
+            public IList<int> InorderTraversal(TreeNode root)
+            {
+
+                List<int> list = new List<int>();
+                InorderHelper(root, list);
+                return list;
+            }
+            private void InorderHelper(TreeNode node, List<int> list)
+            {
+                if (node == null)
+                    return;
+
+                InorderHelper(node.left, list);
+                list.Add(node.val);
+                InorderHelper(node.right, list);
+            }
+        }
+    }
+}
---
+++ Binary Tree Inorder Traversal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using static Binary_Tree_Inorder_Traversal.Binary_Tree_Inorder_Traversal;
+
+namespace Binary_Tree_Inorder_Traversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a binary tree
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.left.left = new TreeNode(4);
+            root.left.right = new TreeNode(5);
+
+            Solution solution = new Solution();
+            IList<int> inorderTraversal = solution.InorderTraversal(root);
+
+            Console.WriteLine("Inorder Traversal: " + string.Join(", ", inorderTraversal));
+        }
+    }
+}
