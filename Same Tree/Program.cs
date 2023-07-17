2c0e67309be526ec526c7e7e5091e86b70830e54
KyeongsooLee <leeksca26@gmail.com>
2023-07-16 23:04:07 -0400

    Same Tree

---
+++ Same Tree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static Same_Tree.Same_Tree;
+
+namespace Same_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the first binary tree
+            Solution.TreeNode p = new Solution.TreeNode(1);
+            p.left = new Solution.TreeNode(2);
+            p.right = new Solution.TreeNode(3);
+
+            // Create the second binary tree
+            Solution.TreeNode q = new Solution.TreeNode(1);
+            q.left = new Solution.TreeNode(2);
+            q.right = new Solution.TreeNode(3);
+
+            Solution solution = new Solution();
+            bool isSame = solution.IsSameTree(p, q);
+
+            Console.WriteLine("Is Same Tree: " + isSame);
+        }
+    }
+}
---
+++ Same Tree/Same Tree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Same_Tree
+{
+    internal class Same_Tree
+    {
+        public class Solution
+        {
+            public class TreeNode {
+                public int val;
+                public TreeNode left;
+                public TreeNode right;
+                public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+                    this.val = val;
+                    this.left = left;
+                    this.right = right;
+                }
+            }
+ 
+            public bool IsSameTree(TreeNode p, TreeNode q)
+            {
+                if (q == null || p == null)
+                {
+                    return p == q;
+                }
+
+                if (p.val != q.val)
+                {
+                    return false;
+                }
+
+                return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+            }
+        }
+    }
+}
