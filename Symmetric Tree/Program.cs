3e5ded1b70f31858fc909cc3c804f0ada3757ec2
KyeongsooLee <leeksca26@gmail.com>
2023-07-17 01:03:35 -0400

    Symmetric Tree

---
+++ Symmetric Tree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Symmetric_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the first symmetric binary tree
+            Symmetric_Tree.TreeNode root1 = new Symmetric_Tree.TreeNode(1);
+            root1.left = new Symmetric_Tree.TreeNode(2);
+            root1.right = new Symmetric_Tree.TreeNode(2);
+            root1.left.left = new Symmetric_Tree.TreeNode(3);
+            root1.left.right = new Symmetric_Tree.TreeNode(4);
+            root1.right.left = new Symmetric_Tree.TreeNode(4);
+            root1.right.right = new Symmetric_Tree.TreeNode(3);
+
+            // Create the second asymmetric binary tree
+            Symmetric_Tree.TreeNode root2 = new Symmetric_Tree.TreeNode(1);
+            root2.left = new Symmetric_Tree.TreeNode(2);
+            root2.right = new Symmetric_Tree.TreeNode(2);
+            root2.left.right = new Symmetric_Tree.TreeNode(3);
+            root2.right.right = new Symmetric_Tree.TreeNode(3);
+
+            Symmetric_Tree.Solution solution = new Symmetric_Tree.Solution();
+
+            bool isSymmetric1 = solution.IsSymmetric(root1);
+            Console.WriteLine("Is Symmetric (Tree 1): " + isSymmetric1);
+
+            bool isSymmetric2 = solution.IsSymmetric(root2);
+            Console.WriteLine("Is Symmetric (Tree 2): " + isSymmetric2);
+        }
+    }
+}
---
+++ Symmetric Tree/Symmetric Tree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Symmetric_Tree
+{
+    internal class Symmetric_Tree
+    {
+        public class TreeNode {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {    
+                this.val = val;      
+                this.left = left;     
+                this.right = right;
+            }
+        }
+ 
+        public class Solution
+        {
+            public bool IsSymmetric(TreeNode root)
+            {
+                if (root == null)
+                    return true;
+
+                return IsMirror(root.left, root.right);
+            }
+            private bool IsMirror(TreeNode node1, TreeNode node2)
+            {
+                if (node1 == null && node2 == null)
+                    return true;
+
+                if (node1 == null || node2 == null)
+                    return false;
+
+                return (node1.val == node2.val)
+                    && IsMirror(node1.left, node2.right)
+                    && IsMirror(node1.right, node2.left);
+            }
+        }
+    }
+}
