39a3eff8a56f318e95c7fd2a05c96de4bf2b20a2
KyeongsooLee <leeksca26@gmail.com>
2023-07-19 22:33:32 -0400

    Convert Sorted Array to Binary Search Tree

---
+++ Convert Sorted Array to Binary Search Tree/Convert Sorted Array to Binary Search Tree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_Sorted_Array_to_Binary_Search_Tree
+{
+    internal class Convert_Sorted_Array_to_Binary_Search_Tree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            public TreeNode SortedArrayToBST(int[] nums)
+            {
+                return ConstructBST(nums, 0, nums.Length - 1);
+            }
+
+            private TreeNode ConstructBST(int[] nums, int left, int right)
+            {
+                if (left > right)
+                {
+                    return null;
+                }
+
+                int mid = left + (right - left) / 2;
+                TreeNode root = new TreeNode(nums[mid]);
+
+                root.left = ConstructBST(nums, left, mid - 1);
+                root.right = ConstructBST(nums, mid + 1, right);
+
+                return root;
+            }
+        }
+    }
+}
---
+++ Convert Sorted Array to Binary Search Tree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static Convert_Sorted_Array_to_Binary_Search_Tree.Convert_Sorted_Array_to_Binary_Search_Tree;
+
+namespace Convert_Sorted_Array_to_Binary_Search_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { -10, -3, 0, 5, 9 };
+
+            Solution solution = new Solution();
+            TreeNode root = solution.SortedArrayToBST(nums);
+
+            PrintTree(root);
+        }
+
+        static void PrintTree(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Console.Write(node.val + ", ");
+
+            PrintTree(node.left);
+            PrintTree(node.right);
+        }
+    }
+}
