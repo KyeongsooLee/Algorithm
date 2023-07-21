af0c5bf92c7cf78f6db6f7479645de7e0b8cc8f7
KyeongsooLee <leeksca26@gmail.com>
2023-07-20 22:15:29 -0400

    Balanced Binary Tree

---
+++ Balanced Binary Tree/Balanced Binary Tree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balanced_Binary_Tree
+{
+    internal class Balanced_Binary_Tree
+    {
+        public class Solution
+        {
+            public class TreeNode {
+                public int val;
+                public TreeNode left;
+                public TreeNode right;
+                public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+                    this.val = val;
+                    this.left = left;
+                    this.right = right;
+                }
+            }
+ 
+            public bool IsBalanced(TreeNode root)
+            {
+                return IsBalancedHelper(root) != -1;
+            }
+            private int IsBalancedHelper(TreeNode node)
+            {
+                if (node == null)
+                    return 0;
+
+                int leftHeight = IsBalancedHelper(node.left);
+                if (leftHeight == -1)
+                    return -1;
+
+                int rightHeight = IsBalancedHelper(node.right);
+                if (rightHeight == -1)
+                    return -1;
+
+                if (Math.Abs(leftHeight - rightHeight) > 1)
+                    return -1;
+
+                return Math.Max(leftHeight, rightHeight) + 1;
+            }
+        }
+    }
+}
---
+++ Balanced Binary Tree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static Balanced_Binary_Tree.Balanced_Binary_Tree;
+
+namespace Balanced_Binary_Tree
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Creating the binary tree for example 1: [3, 9, 20, null, null, 15, 7]
+            Solution.TreeNode example1Root = new Solution.TreeNode(3,
+                new Solution.TreeNode(9),
+                new Solution.TreeNode(20,
+                    new Solution.TreeNode(15),
+                    new Solution.TreeNode(7)
+                )
+            );
+
+            // Creating an instance of the Solution class
+            Solution solution = new Solution();
+
+            // Checking if the binary tree is height-balanced
+            bool isBalanced = solution.IsBalanced(example1Root);
+
+            // Outputting the result
+            Console.WriteLine("Is the binary tree height-balanced? " + isBalanced);
+        }
+    }
+}
