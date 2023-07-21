9413b9c9f95212eb121689927ad62e8bdb2be4db
KyeongsooLee <leeksca26@gmail.com>
2023-07-21 01:54:44 -0400

    Minimum Depth of Binary Tree

---
+++ Minimum Depth of Binary Tree/Minimum Depth of Binary Tree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimum_Depth_of_Binary_Tree
+{
+    internal class Minimum_Depth_of_Binary_Tree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public class Solution
+        {
+            public int MinDepth(TreeNode root)
+            {
+                if (root == null)
+                    return 0;
+
+                Queue<TreeNode> queue = new Queue<TreeNode>();
+                queue.Enqueue(root);
+                int depth = 1;
+
+                while (queue.Count > 0)
+                {
+                    int levelSize = queue.Count;
+
+                    for (int i = 0; i < levelSize; i++)
+                    {
+                        TreeNode currentNode = queue.Dequeue();
+
+                        // Check if it's a leaf node
+                        if (currentNode.left == null && currentNode.right == null)
+                        {
+                            return depth;
+                        }
+                        if (currentNode.left != null)
+                        {
+                            queue.Enqueue(currentNode.left);
+                        }
+                        if (currentNode.right != null)
+                        {
+                            queue.Enqueue(currentNode.right);
+                        }
+                    }
+                    depth++;
+                }
+
+                return depth;
+            }
+        }
+    }
+}
---
+++ Minimum Depth of Binary Tree/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static Minimum_Depth_of_Binary_Tree.Minimum_Depth_of_Binary_Tree;
+
+namespace Minimum_Depth_of_Binary_Tree
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Example 1: [3, 9, 20, null, null, 15, 7]
+            TreeNode example1Root = new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20, 
+                new TreeNode(15), 
+                new TreeNode(7))
+            );
+
+            Solution solution = new Solution();
+            int minDepthExample1 = solution.MinDepth(example1Root);
+            Console.WriteLine("Minimum depth of Example 1: " + minDepthExample1); // Output: 2
+
+            // Example 2: [2, null, 3, null, 4, null, 5, null, 6]
+            TreeNode example2Root = new TreeNode(2,
+                null,
+                new TreeNode(3,
+                null,
+                new TreeNode(4,
+                null,
+                new TreeNode(5,
+                null,
+                new TreeNode(6)
+                ))));
+
+            int minDepthExample2 = solution.MinDepth(example2Root);
+            Console.WriteLine("Minimum depth of Example 2: " + minDepthExample2); // Output: 5
+        }
+    }
+}
