75654988080dae78a1f1898456ef078fb88a43fc
KyeongsooLee <leeksca26@gmail.com>
2023-07-22 00:34:42 -0400

    Path Sum

---
+++ Path Sum/Path Sum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Path_Sum
+{
+    internal class Path_Sum
+    {
+        public class Solution
+        {
+            public class TreeNode
+            {
+                public int val;
+                public TreeNode left;
+                public TreeNode right;
+                public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+                {
+                    this.val = val;
+                    this.left = left;
+                    this.right = right;
+                }
+            }
+            public bool HasPathSum(TreeNode root, int targetSum)
+            {
+                if (root == null)
+                {
+                    return false; // Empty tree has no root-to-leaf path.
+                }
+
+                // Check if the current node is a leaf node and if its value matches the targetSum.
+                if (root.left == null && root.right == null && root.val == targetSum)
+                {
+                    return true;
+                }
+
+                // Recursively check the left and right subtrees.
+                return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+            }
+        }
+    }
+}
---
+++ Path Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static Path_Sum.Path_Sum;
+using static Path_Sum.Path_Sum.Solution;
+
+namespace Path_Sum
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Create the binary tree as described in the examples.
+            TreeNode root = new TreeNode(5,
+                new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(2))),
+                new TreeNode(8, new TreeNode(13), new TreeNode(4, null, new TreeNode(1)))
+            );
+
+            int targetSum = 22;
+
+            Solution solution = new Solution();
+            bool hasPathSum = solution.HasPathSum(root, targetSum);
+
+            Console.WriteLine("Has root-to-leaf path with sum " + targetSum + ": " + hasPathSum);
+        }
+    }
+}
