3acef0a0c2a23b5da3211327e90bb363e18bb08d
KyeongsooLee <leeksca26@gmail.com>
2023-07-23 00:41:44 -0400

    Single Number

---
+++ Single Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static Single_Number.Single_Number;
+
+namespace Single_Number
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int[] nums1 = { 2, 2, 1 };
+            int result1 = solution.SingleNumber(nums1);
+            Console.WriteLine(result1); // Output: 1
+
+            int[] nums2 = { 4, 1, 2, 1, 2 };
+            int result2 = solution.SingleNumber(nums2);
+            Console.WriteLine(result2); // Output: 4
+
+            int[] nums3 = { 1 };
+            int result3 = solution.SingleNumber(nums3);
+            Console.WriteLine(result3); // Output: 1
+        }
+    }
+}
---
+++ Single Number/Single Number.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Number
+{
+    internal class Single_Number
+    {
+        public class Solution
+        {
+            public int SingleNumber(int[] nums)
+            {
+                int result = 0;
+
+                foreach (int num in nums)
+                {
+                    result ^= num; // XOR
+                }
+
+                return result;
+            }
+        }
+    }
+}
