7af58d3a444fbbed12bc13743c5e62035f641489
KyeongsooLee <leeksca26@gmail.com>
2023-07-25 00:20:54 -0600

    Valid Palindrome

---
+++ Valid Palindrome/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static Valid_Palindrome.Valid_Palindrome;
+
+namespace Valid_Palindrome
+{
+    class Program
+    {
+        static void Main()
+        {
+            Solution solution = new Solution();
+
+            string s1 = "A man, a plan, a canal: Panama";
+            Console.WriteLine(solution.IsPalindrome(s1)); // Output: True
+
+            string s2 = "race a car";
+            Console.WriteLine(solution.IsPalindrome(s2)); // Output: False
+
+            string s3 = " "; // An empty string is considered a palindrome
+            Console.WriteLine(solution.IsPalindrome(s3)); // Output: True
+        }
+    }
+}
---
+++ Valid Palindrome/Valid Palindrome.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_Palindrome
+{
+    internal class Valid_Palindrome
+    {
+        public class Solution
+        {
+            public bool IsPalindrome(string s)
+            {
+                // Convert the string to lowercase
+                s = s.ToLower();
+
+                // Use StringBuilder to create a new string with alphanumeric characters only
+                StringBuilder sb = new StringBuilder();
+                foreach (char c in s)
+                {
+                    if (char.IsLetterOrDigit(c))
+                        sb.Append(c);
+                }
+
+                // Get the cleaned string after removing non-alphanumeric characters
+                string cleanedString = sb.ToString();
+
+                // Check if the cleaned string is a palindrome
+                int left = 0;
+                int right = cleanedString.Length - 1;
+
+                while (left < right)
+                {
+                    if (cleanedString[left] != cleanedString[right])
+                    {
+                        return false;
+                    }
+                    left++;
+                    right--;
+                }
+
+                return true;
+            }
+        }
+    }
+}
