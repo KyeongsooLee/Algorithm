2534cd231284a747cce812af0832fed2e15514d6
KyeongsooLee <leeksca26@gmail.com>
2023-07-24 22:51:08 -0600

    Linked List Cycle

---
+++ Linked List Cycle/Linked List Cycle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List_Cycle
+{
+    internal class Linked_List_Cycle
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+        public class Solution
+        {
+            public bool HasCycle(ListNode head)
+            {
+                if (head == null || head.next == null)
+                    return false;
+
+                ListNode slow = head;
+                ListNode fast = head;
+
+                while (fast != null && fast.next != null)
+                {
+                    slow = slow.next;
+                    fast = fast.next.next;
+
+                    if (slow == fast)
+                        return true;
+                }
+
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Linked List Cycle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using static Linked_List_Cycle.Linked_List_Cycle;
+
+namespace Linked_List_Cycle
+{
+    class Program
+    {
+        static void Main()
+        {
+            Solution solution = new Solution();
+
+            // Example 1: [3,2,0,-4] with pos = 1
+            ListNode node4 = new ListNode(-4);
+            ListNode node3 = new ListNode(0);
+            ListNode node2 = new ListNode(2);
+            ListNode node1 = new ListNode(3);
+
+            node1.next = node2;
+            node2.next = node3;
+            node3.next = node4;
+            node4.next = node2; // creates the cycle
+
+            bool result1 = solution.HasCycle(node1);
+            Console.WriteLine("Example 1: " + result1); // Output: true
+
+            // Example 2: [1,2] with pos = 0
+            ListNode node5 = new ListNode(1);
+            ListNode node6 = new ListNode(2);
+
+            node5.next = node6;
+            node6.next = node5; // creates the cycle
+
+            bool result2 = solution.HasCycle(node5);
+            Console.WriteLine("Example 2: " + result2); // Output: true
+
+            // Example 3: [1] with pos = -1
+            ListNode node7 = new ListNode(1);
+
+            bool result3 = solution.HasCycle(node7);
+            Console.WriteLine("Example 3: " + result3); // Output: false
+        }
+    }
+}
