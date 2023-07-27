e8ac93e005c90123731757921db4095392d0ee1d
KyeongsooLee <leeksca26@gmail.com>
2023-07-26 22:55:53 -0600

    Majority Element

---
+++ Majority Element/Majority Element.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Majority_Element
+{
+    internal class Majority_Element
+    {
+        public class Solution
+        {
+            public int MajorityElement(int[] nums)
+            {
+                int count = 0;
+                int? candidate = null;
+
+                foreach (int num in nums)
+                {
+                    if (count == 0) candidate = num;
+                    count += (num == candidate) ? 1 : -1;
+                }
+
+                return candidate.Value;
+            }
+        }
+    }
+}
---
+++ Majority Element/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static Majority_Element.Majority_Element;
+
+namespace Majority_Element
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            // Test case 1
+            int[] nums1 = { 3, 2, 3 };
+            int result1 = solution.MajorityElement(nums1);
+            Console.WriteLine("Test case 1: Majority element is " + result1);
+
+            // Test case 2
+            int[] nums2 = { 2, 2, 1, 1, 1, 2, 2 };
+            int result2 = solution.MajorityElement(nums2);
+            Console.WriteLine("Test case 2: Majority element is " + result2);
+        }
+    }
+}
