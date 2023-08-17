2e003ccfe6f5108bcbad643c7989d046669e8921
KyeongsooLee <leeksca26@gmail.com>
2023-08-16 20:52:46 -0400

    Reverse Bits

---
+++ Reverse Bits/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static Reverse_Bits.Reverse_Bits;
+
+namespace Reverse_Bits
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            // Test the provided examples
+            uint test1 = 43261596; // Corresponds to binary: 00000010100101000001111010011100
+            uint test2 = 4294967293; // Corresponds to binary: 11111111111111111111111111111101
+
+            uint result1 = solution.reverseBits(test1);
+            uint result2 = solution.reverseBits(test2);
+
+            Console.WriteLine($"Input: {test1} ({Convert.ToString(test1, 2).PadLeft(32, '0')}), Output: {result1} ({Convert.ToString(result1, 2).PadLeft(32, '0')})");
+            Console.WriteLine($"Input: {test2} ({Convert.ToString(test2, 2).PadLeft(32, '0')}), Output: {result2} ({Convert.ToString(result2, 2).PadLeft(32, '0')})");
+        }
+    }
+}
---
+++ Reverse Bits/Reverse Bits.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Bits
+{
+    internal class Reverse_Bits
+    {
+        public class Solution
+        {
+            public uint reverseBits(uint n)
+            {
+                uint result = 0;
+                for (int i = 0; i < 32; i++)
+                {
+                    result <<= 1; // shift left to make space for the next bit
+                    if ((n & 1) == 1)
+                    { // if the current bit is set
+                        result |= 1; // set the bit in the result
+                    }
+                    n >>= 1; // move to the next bit in n
+                }
+                return result;
+            }
+        }
+    }
+}
